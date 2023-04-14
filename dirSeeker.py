02b3d8d16a612fdc47b8ef72b4fcae5ac42dfce5
FulPlan Mutasim <mutasimplan@protonmail.com>
2023-04-14 20:03:08 +0100

    Its All Good

--- dirSeeker.py
+++ dirSeeker.py
@@ -4,9 +4,10 @@ import queue
 import random
 import re
 import requests
+import socket
 import sys
 import threading
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 
 class DirSeeker:
@@ -14,7 +15,7 @@ class DirSeeker:
         self.args = None
         self.directories = []
         self.responses = []
-        self.extensions = None
+        self.extensions = {'.php', '.html', '.htm', '.txt', '.md'}
         self.queue = queue.Queue()
         self.threads = []
         self.user_agents = [
@@ -22,11 +23,28 @@ class DirSeeker:
             "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
         ]
+        self.color_codes = {
+            200: "\033[92m",  # green
+            201: "\033[92m",  # green
+            202: "\033[92m",  # green
+            204: "\033[92m",  # green
+            301: "\033[96m",  # cyan
+            302: "\033[96m",  # cyan
+            307: "\033[96m",  # cyan
+            308: "\033[96m",  # cyan
+            401: "\033[93m",  # yellow
+            403: "\033[93m",  # yellow
+            404: "\033[91m",  # red
+            405: "\033[91m",  # red
+            500: "\033[91m",  # red
+            501: "\033[91m"  # red
+        }
 
     # Setup argument parser
     def setup_parser(self):
         parser = argparse.ArgumentParser(description="Advance DirSeeker Tool")
-        parser.add_argument('--target', help='Target URL', required=True)
+        parser.add_argument(
+            '--target', help='Target URL or IP address', required=True)
         parser.add_argument(
             '--wordlist', help='Path to wordlist file', required=True)
         parser.add_argument(
@@ -45,11 +63,21 @@ class DirSeeker:
             '--color', help='Display color results', action='store_true')
         self.args = parser.parse_args()
 
-    # Load the wordlist file
+    # Load the wordlist file and append extensions as files
     def load_wordlist(self):
         try:
             with open(self.args.wordlist, 'r') as wordlist_file:
-                self.directories = [line.rstrip() for line in wordlist_file]
+                for line in wordlist_file:
+                    dir_or_ext = line.rstrip()
+                    if '.' not in dir_or_ext:
+                        for ext in self.extensions:
+                            filepath = f"{dir_or_ext}{ext}"
+                            if os.path.isfile(filepath):
+                                self.directories.append(filepath)
+                    else:
+                        if os.path.isfile(dir_or_ext):
+                            self.directories.append(dir_or_ext)
+
         except FileNotFoundError:
             print("Wordlist file not found.")
             sys.exit()
@@ -70,24 +98,46 @@ class DirSeeker:
 
         return True
 
-    # Check if extension match
+    # Check if extension matches the specified extensions
     def check_extension(self, url):
-        if self.extensions:
-            for ext in self.extensions:
-                if url.endswith(ext):
-                    return True
-            return False
-        else:
+        if not self.extensions:
             return True
 
-    # Send request to the target URL
+        parsed_url = urlparse(url)
+        path_components = os.path.splitext(parsed_url.path)
+
+        for ext in self.extensions:
+            if path_components[1] == f".{ext}":
+                return True
+
+        return False
+
+    # Send HTTP request to the target
     def send_request(self):
         while True:
             directory = self.queue.get()
-            if not directory:
+            if directory is None:
                 break
 
-            url = urljoin(self.args.target, directory)
+            url = urlparse(self.args.target)
+
+            if not url.scheme:
+                url = url._replace(scheme='http')
+
+            if not url.netloc:
+                try:
+                    socket.inet_aton(url.path)
+                    url = url._replace(netloc=url.path)
+                    url = url._replace(path='')
+                except socket.error:
+                    url = url._replace(netloc=url.path + '.com')
+                    url = url._replace(path='')
+
+            if not url.path:
+                url = url._replace(path='/')
+
+            url = urljoin(url.geturl(), directory)
+
             headers = {}
             if self.args.random_agent:
                 user_agent = random.choice(self.user_agents)
@@ -98,7 +148,10 @@ class DirSeeker:
                 self.responses.append((url, response))
                 if self.filter_codes(response) and self.check_extension(url):
                     if self.args.color:
-                        print(f"\033[92m[{response.status_code}] {url}\033[0m")
+                        color_code = self.color_codes.get(
+                            response.status_code, "")
+                        print(
+                            f"{color_code}[{response.status_code}] {url}\033[0m")
                     else:
                         print(f"[{response.status_code}] {url}")
             except (requests.exceptions.RequestException, requests.exceptions.Timeout):
@@ -139,7 +192,8 @@ class DirSeeker:
             self.responses.append((url, response))
             if self.filter_codes(response) and self.check_extension(url):
                 if self.args.color:
-                    print(f"\033[92m[{response.status_code}] {url}\033[0m")
+                    color_code = self.color_codes.get(response.status_code, "")
+                    print(f"{color_code}[{response.status_code}] {url}\033[0m")
                 else:
                     print(f"[{response.status_code}] {url}")
         except (requests.exceptions.RequestException, requests.exceptions.Timeout):
@@ -175,10 +229,18 @@ class DirSeeker:
         self.setup_parser()
 
         if self.args.extensions:
-            self.extensions = [ext.strip()
-                               for ext in self.args.extensions.split(',')]
+            self.extensions.update(ext.strip()
+                                   for ext in self.args.extensions.split(','))
 
         self.load_wordlist()
+
+        try:
+            socket.inet_aton(self.args.target)
+            self.args.target = f"http://{self.args.target}"
+        except socket.error:
+            parsed_url = urlparse(self.args.target)
+            if not parsed_url.scheme or not parsed_url.netloc:
+                self.args.target = f"http://{self.args.target}.com"
 
         if self.args.recursive:
             self.scan_recursive_directories()
@@ -191,5 +253,4 @@ if __name__ == '__main__':
         dir_search = DirSeeker()
         dir_search.run()
     except KeyboardInterrupt:
-        print("\nExiting program...\n")
-        sys.exit()
+        sys.exit("\nExiting on user request.")
